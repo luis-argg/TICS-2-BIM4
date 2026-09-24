1e63fcd4ba4130572f223a8fcdc1cd4f6796291f
LuisArgueta <largueta-2025599@kinal.edu.gt>
2026-09-24 16:13:23 -0600

    Ultimos cambios para que la actividad 2 este realizada

--- js/script.js
+++ js/script.js
@@ -2,8 +2,23 @@ const titulo = document.getElementById('titulo');
 const parrafo = document.getElementById('parrafo');
 const boton = document.getElementById('boton');
 const extra = document.querySelector('.extra');
+const imagen = document.getElementById('imagen');
+const reiniciar = document.getElementById('reiniciar');
+
+const textoInicialParrafo = parrafo.textContent;
+const textoInicialExtra = extra.textContent;
+const srcImagenInicial = imagen.src;
+const srcImagenGif = 'https://media1.tenor.com/m/JxH03vCBQLwAAAAd/rei-chiquita-rei-dance.gif';
 
 let colorAlterno = false;
+
+function reiniciarTodo() {
+    colorAlterno = false;
+    titulo.style.color = '#AD280E';
+    parrafo.textContent = textoInicialParrafo;
+    extra.textContent = textoInicialExtra;
+    imagen.src = srcImagenInicial;
+}
 
 boton.addEventListener('click', function () {
     if (colorAlterno) {
@@ -29,4 +44,13 @@ titulo.addEventListener('click', function () {
 
 parrafo.addEventListener('dblclick', function () {
     parrafo.textContent = 'Has cambiado el parrafo con doble cliick ';
+});
+
+imagen.addEventListener('click', function () {
+    imagen.src = srcImagenGif;
+});
+
+reiniciar.addEventListener('click', function () {
+    reiniciarTodo();
+    alert('Eventos reiniciados y la imagen volvio a la normalidad ');
 });
